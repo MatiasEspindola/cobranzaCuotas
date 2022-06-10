670ad8008aaf6c8c7c94d0febf158944e6f96619
MatiasEspindola <matiasespindolarp@hotmail.com>
2022-06-10 15:17:29 -0300

    Actualizacion 10-06-2022

---
+++ src/main/java/com/proyectoCuotasRyR/proyectoCuotas/models/repo/I_Authority_Repo.java
@@ -0,0 +1,9 @@
+package com.proyectoCuotasRyR.proyectoCuotas.models.repo;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.proyectoCuotasRyR.proyectoCuotas.models.entities.Authority;
+
+public interface I_Authority_Repo extends CrudRepository<Authority, Long> {
+
+}
--- src/main/java/com/proyectoCuotasRyR/proyectoCuotas/models/repo/I_Respuesta_Repo.java
+++
@@ -1,9 +0,0 @@
-package com.proyectoCuotasRyR.proyectoCuotas.models.repo;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.proyectoCuotasRyR.proyectoCuotas.models.entities.Respuesta;
-
-public interface I_Respuesta_Repo extends CrudRepository<Respuesta, Long> {
-
-}
